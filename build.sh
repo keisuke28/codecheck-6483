1aaa61401133f19a7a4e4eb1d33d8bc12e6b8fba
code-project <dev@code-check.io>
2017-02-23 11:17:19 +0000

    Overwrite original challenge files

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 
 ROOT=$(cd $(dirname $0) && pwd)
 
+### Java ###
+# cd $ROOT/java/
+# mvn package -Dmaven.test.skip=true
+
+### Scala ###
+# cd $ROOT/scala/
+# mvn package -Dmaven.test.skip=true
+
+### Python ###
+# cd $ROOT/python/src
+# python -m compileall .
 
 ### C++ ###
- cd $ROOT/c++
- c++ -o app.o src/*.cpp
+# cd $ROOT/c++
+# c++ -o app.o src/*.cpp
--- run.sh
+++ run.sh
@@ -2,7 +2,17 @@
 
 ROOT=$(cd $(dirname $0) && pwd)
 
+### Java ###
+# java -jar $(ls $ROOT/java/target/ce-exam-*-jar-with-dependencies.jar) "$@"
 
+### Scala ###
+# java -jar $(ls $ROOT/scala/target/ce-exam-*-jar-with-dependencies.jar) "$@"
+
+### Python ###
+# python $ROOT/python/src/main.pyc "$@"
+
+### NodeJS ###
+# node $ROOT/nodejs/src/main.js "$@"
 
 ### C++ ###
-$ROOT/c++/app.o "$@"
+# $ROOT/c++/app.o "$@"
